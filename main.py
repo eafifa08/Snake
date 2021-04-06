57a8f2f74ee62650c7758d40392392e87865d8ca
eafifa08 <eafifa07@yandex.ru>
2021-04-06 23:02:07 +0500

    beginning

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
+import sys
+
+FPS = 60
+PATH_TO_DATABASE = "snake.sqlite"
 
 pygame.init()
+clock = pygame.time.Clock()
+pygame.display.set_caption('Snake')
+screen = pygame.display.set_mode((500, 300))
+screen.fill(pygame.color.THECOLORS['grey'])
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
