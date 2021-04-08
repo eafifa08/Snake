42dbddc05dbce05b28be2e5d2dd554d8e37862b9
eafifa08 <eafifa07@yandex.ru>
2021-04-08 17:42:02 +0500

    field

--- Logic.py
+++ Logic.py
@@ -1,20 +1,33 @@
 class Snake:
-    def __init__(self, name, length, life, speed, armor):
+    def __init__(self, name, length, life, speed, armor, resizable):
+        """
+        Змея
+        :param name: имя
+        :param length: длина
+        :param life: количество жизней
+        :param speed: скорость
+        :param armor: броня
+        """
         self.name = name
         self.length = length
         self.life = life
         self.speed = speed
         self.armor = armor
-
+        self.resizable = resizable
 
 
 class Field:
     def __init__(self, size, dangerous_walls, something_good_max):
+        """
+        Поле
+        :param size: ширина,высота
+        :param dangerous_walls: столкновение со стеной приводит к гибели
+        :param something_good_max: макс.количество полезных сущностей на поле
+        """
         self.width, self.height = size
         self.dangerous_walls = dangerous_walls
         self.something_good_max = something_good_max
         self.something_good_min = 0
-        self.something_good
 
 
 
--- main.py
+++ main.py
@@ -3,12 +3,20 @@ import sys
 
 FPS = 60
 PATH_TO_DATABASE = "snake.sqlite"
+SCREEN_SIZE = (200, 200)
+CELL_SIZE = 20
 
 pygame.init()
 clock = pygame.time.Clock()
 pygame.display.set_caption('Snake')
-screen = pygame.display.set_mode((500, 300))
+screen = pygame.display.set_mode(SCREEN_SIZE)
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen.fill(pygame.color.THECOLORS['grey'])
+for i in range(0, SCREEN_SIZE[0]):
+    pygame.draw.line(screen, pygame.color.THECOLORS['blue'], (i*CELL_SIZE, 0), (i*CELL_SIZE, SCREEN_SIZE[1]), 2)
+for i in range(0, SCREEN_SIZE[1]):
+    pygame.draw.line(screen, pygame.color.THECOLORS['blue'], (0, i*CELL_SIZE), (SCREEN_SIZE[0], i*CELL_SIZE), 2)
+
 pygame.display.update()
 
 while True:
