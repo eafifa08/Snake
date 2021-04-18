3ab2858e6d01a11858ec3c6a4cb346cbb876bfdb
eafifa08 <eafifa07@yandex.ru>
2021-04-18 21:04:43 +0500

    hi

--- Logic.py
+++ Logic.py
@@ -3,6 +3,7 @@ import random
 
 WIDTH = 400
 HEIGHT = 400
+CELL_SIZE = 20
 
 
 class Snake(pygame.sprite.Sprite):
@@ -16,11 +17,19 @@ class Snake(pygame.sprite.Sprite):
         :param armor: броня
         """
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((20, 20))
-        self.image.fill(pygame.color.THECOLORS['green'])
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE*3), pygame.SRCALPHA, 32)
+        #self.image = pygame.image.load('media\snake_head.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.centerx = 0
-        self.rect.bottom = 0
+        self.rect.left = WIDTH // 2
+        self.rect.bottom = HEIGHT // 2
+
+        self.head_coordinates = [0, CELL_SIZE*2]
+        self.body_coordinates = [self.head_coordinates]
+        self.polygon_coordinates = [[0, 0], [0, CELL_SIZE*3], [CELL_SIZE, CELL_SIZE*3], [CELL_SIZE, 0]]
+
+        pygame.draw.polygon(self.image, pygame.color.THECOLORS['red'], self.polygon_coordinates)
+        self.mask = pygame.mask.from_surface(self.image)
+        #self.image.fill(pygame.color.THECOLORS['green'])
         self.direction = 'down'
         self.name = name
         self.length = length
@@ -32,25 +41,30 @@ class Snake(pygame.sprite.Sprite):
         self.eaten = 0
 
 
-    def update(self):
-        #self.speed = 0
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.speed_x = -5
-            self.speed_y = 0
-            self.direction = 'left'
-        if keystate[pygame.K_RIGHT]:
-            self.speed_x = 5
-            self.speed_y = 0
-            self.direction = 'right'
-        if keystate[pygame.K_DOWN]:
-            self.speed_y = 5
-            self.speed_x = 0
-            self.direction = 'down'
-        if keystate[pygame.K_UP]:
-            self.speed_y = -5
-            self.speed_x = 0
-            self.direction = 'up'
+
+    def update(self, direction):
+
+
+        if direction == 'up':
+            if self.direction != 'down':
+                self.speed_y = -20
+                self.speed_x = 0
+                self.direction = 'up'
+        if direction == 'down':
+            if self.direction != 'up':
+                self.speed_y = 20
+                self.speed_x = 0
+                self.direction = 'down'
+        if direction == 'left':
+            if self.direction != 'right':
+                self.speed_y = 0
+                self.speed_x = -20
+                self.direction = 'left'
+        if direction == 'right':
+            if self.direction != 'left':
+                self.speed_y = 0
+                self.speed_x = 20
+                self.direction = 'right'
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
         if self.rect.right > WIDTH:
@@ -61,6 +75,8 @@ class Snake(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
+
+
 
 class Field:
     def __init__(self, size, dangerous_walls, something_good_max):
@@ -82,8 +98,8 @@ class Food(pygame.sprite.Sprite):
         self.image = pygame.Surface((10, 10))
         self.image.fill(pygame.color.THECOLORS['blue'])
         self.rect = self.image.get_rect()
-        self.rect.centerx = random.randint(0, 400)
-        self.rect.bottom = random.randint(0, 400)
+        self.rect.left = random.randint(1, 399) // 20 * 20
+        self.rect.bottom = random.randint(1, 399) // 20 * 20
         self.eaten = False
         self.deleted = False
 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame_menu
 import sys
 import Logic
 
-FPS = 10
+FPS = 2
 PATH_TO_DATABASE = "snake.sqlite"
 SCREEN_SIZE = (400, 400)
 CELL_SIZE = 20
@@ -31,26 +31,38 @@ def start_the_game(ROUND_MAX_FOOD):
     pygame.display.update()
     all_sprite = pygame.sprite.Group()
     food_sprite = pygame.sprite.Group()
-    snake = Logic.Snake(0, 0, 0, 0, 0, 0, 0)
+    snake = Logic.Snake(0, 3, 0, 0, 0, 0, 0)
     foods = []
     for i in range(MAX_FOOD):
         foods.append(Logic.Food())
         food_sprite.add(foods[i])
     all_sprite.add(snake)
+    direction = 'down'
 
     while ROUND_MAX_FOOD > 0:
+
         clock.tick(FPS)
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 print('escape')
                 return None
+            if event.type == pygame.KEYDOWN:
+                print(str(event.key))
+                if event.key == pygame.K_UP:
+                    direction = 'up'
+                if event.key == pygame.K_DOWN:
+                    direction = 'down'
+                if event.key == pygame.K_LEFT:
+                    direction = 'left'
+                if event.key == pygame.K_RIGHT:
+                    direction = 'right'
 
 
-
-        all_sprite.update()
+        all_sprite.update(direction)
         food_sprite.update(snake)
 
         screen.fill(pygame.color.THECOLORS['grey'])
@@ -64,6 +76,7 @@ def start_the_game(ROUND_MAX_FOOD):
         all_sprite.draw(screen)
         food_sprite.draw(screen)
         pygame.display.flip()
+        pygame.display.update()
         for i in range(0, len(foods)):
             if foods[i].eaten and foods[i].deleted == False:
                 food = Logic.Food()
